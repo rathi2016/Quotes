b3dba7932841229db4c65173a2664f60813ecd81
Rathi <rathind14@gmail.com>
2018-12-03 12:43:50 -0500

    Retrofit call to the api done success

--- app/src/main/java/workhardcom/rathind/soulquotes/MainActivity.java
+++ app/src/main/java/workhardcom/rathind/soulquotes/MainActivity.java
@@ -2,6 +2,12 @@ package workhardcom.rathind.soulquotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getData();
+    }
+
+
+    private void getData()
+    {
+        Call<QuoteOfTheDay> quoteOfTheDayCall = QuotesApi.getQuotesService().getContents();
+        quoteOfTheDayCall.enqueue(new Callback<QuoteOfTheDay>() {
+            @Override
+            public void onResponse(Call<QuoteOfTheDay> call, Response<QuoteOfTheDay> response) {
+                QuoteOfTheDay quote1 = response.body();
+                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<QuoteOfTheDay> call, Throwable t) {
+
+            }
+        });
     }
 }
--- app/src/main/java/workhardcom/rathind/soulquotes/QuotesApi.java
+++ app/src/main/java/workhardcom/rathind/soulquotes/QuotesApi.java
@@ -1,5 +1,39 @@
 package workhardcom.rathind.soulquotes;
 
+import com.google.gson.Gson;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
 public class QuotesApi {
-    private static final String url= "http://quotes.rest/qod.json";
+    private static final String url= "http://quotes.rest/";
+    public static QuotesService quotesService = null;
+
+
+    public static  QuotesService getQuotesService()
+    {
+        if(quotesService == null)
+        {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(url)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            quotesService = retrofit.create(QuotesService.class);
+        }
+        return quotesService;
+
+
+    }
+
+
+
+ public interface QuotesService{
+     @GET("qod.json")
+     Call<QuoteOfTheDay> getContents();
+
+ }
+
 }
